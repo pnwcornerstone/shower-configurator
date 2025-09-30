1c84b9d35ac8f892231c6e8c6cdddb69e85eb6bd
pnwcornerstone <samuel@pnwcornerstone.com>
2025-09-30 14:30:50 -0700

    script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const showerImage = document.getElementById("shower-image");
-  const offset = document.getElementById("offset");
-  const layout = document.getElementById("layout");
-  const insert = document.getElementById("insert");
-
-  function updateImage() {
-    // Build a filename from options
-    const fileName = `${offset.value}-${layout.value}-${insert.value}.png`;
-    showerImage.src = `images/${fileName}`;
-  }
-
-  offset.addEventListener("change", updateImage);
-  layout.addEventListener("change", updateImage);
-  insert.addEventListener("change", updateImage);
-});
+console.log("Shower Configurator Loaded");
